a2a98346ad6d694336fecebf629f5bd2c030cf99
Sam <alisherkodirov07@gmail.com>
2025-02-08 19:36:40 +0500

    Working on it...

---
+++ js/theme.js
@@ -0,0 +1,24 @@
+const themeLink = document.getElementById("theme")
+
+function changeTip(){
+    let tipCHanged = false
+    return function changeTipText(){
+        let tipText = document.getElementById("tipText")
+        if(!tipCHanged){
+            tipText.innerText = "Yup... You got it! ;)"
+            tipCHanged = !tipCHanged
+        }
+    }
+}
+
+let changeTipText = changeTip()
+
+window.addEventListener("click", (e) => {
+    let currentTheme = themeLink.getAttribute("href")
+    if(currentTheme === "./css/theme-dark.css") {
+        themeLink.setAttribute("href", "./css/theme-light.css")
+    }else {
+        themeLink.setAttribute("href", "./css/theme-dark.css")
+    }
+    changeTipText()
+})
